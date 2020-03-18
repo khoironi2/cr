37543162802f1e90340e3f635bbef6c68869924b
khoironi <masrony37@gmail.com>
2020-03-19 02:43:59 +0700

    product

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -5,15 +5,29 @@ class Product extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = 'Product';
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
 
-        $data['title'] = 'Product';
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('ujian/index', $data);
-        $this->load->view('templates/footer');
+        $data['menu'] = $this->M_menu->get();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        if ($this->form_validation->run() == false) {
+
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('product/index', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                    New menu added.
+                    </div>');
+            redirect('menu');
+        }
     }
 }
